ca21c12f1f022b8209ccd0229dde4d69e51acbe2
DaisyAram <138888608+DaisyAram@users.noreply.github.com>
2024-09-19 19:19:00 +0300

    delete url folder

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import Navigation from "./Footer/Header/Header";
+import Navigation from "./Footer/Homepage/Header/Header";
 import Homepage from "./Footer/Homepage/Homepage";
-import Hero from "./Footer/Header/Hero/Hero";
+import Hero from "./Footer/Homepage/Header/Hero/Hero";
 import Footer from "./Footer/Footer";
 function App() {
   return (
